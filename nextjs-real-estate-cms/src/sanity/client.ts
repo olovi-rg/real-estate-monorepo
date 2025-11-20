7bf0aabcdaa570086449a59a10d747a13cc88512
olovi-rg <olovirg@gmail.com>
2025-11-21 03:38:07 +0530

    Fix Sanity client for Vercel

--- nextjs-real-estate-cms/src/sanity/client.ts
+++ nextjs-real-estate-cms/src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "d6pl5xs5",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-01-01",
   useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN,
